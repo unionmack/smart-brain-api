7170fd3ff82bd228729fd026a20c5b13d0ad4068
Mack Hayden <unionmack@gmail.com>
2020-07-21 23:06:49 -0700

    debugging image typo

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => {Image.handleImage(req,res,db)})
+app.put('/image', (req, res) => {image.handleImage(req,res,db)})
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`app is running on port ${process.env.PORT}`);
