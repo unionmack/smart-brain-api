c5904e085ead24f5bfa0ecd417188c64e1f2d8e0
Mack Hayden <unionmack@gmail.com>
2020-07-21 21:58:28 -0700

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-objective-95122',
       user : 'mackhayden',
       password : '',
       database : 'smart-brain'
